f07cb5b71fb9918a148ae117efe971feb6e55ce3
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2022-12-13 17:34:42 -0500

    updated models index with table associations

--- models/index.js
+++ models/index.js
@@ -5,15 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.hasMany(Category, {
-  
+Category.belongsTo(Product, {
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 module.exports = {
   Product,
