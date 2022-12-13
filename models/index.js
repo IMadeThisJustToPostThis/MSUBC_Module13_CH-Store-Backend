e3707dc734574767cd4304d3df1a3bdfb232bb0e
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2022-12-13 17:39:08 -0500

    association edit

--- models/index.js
+++ models/index.js
@@ -5,8 +5,9 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Category.belongsTo(Product, {
-  foreignKey: 'category_id'
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
 // Categories have many Products
