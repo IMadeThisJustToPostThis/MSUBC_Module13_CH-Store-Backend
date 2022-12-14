7ec562191e7135ddc7b8722bec3b2e1981652f80
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2022-12-13 21:03:18 -0500

    added tag routes

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -12,20 +13,21 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE'
+  foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  as: 'product_tags',
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'product_id'
+  as: 'product_tags',
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
@@ -34,3 +36,43 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+/*
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id',
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'tag_id',
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
+*/
